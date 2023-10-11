5bdbf94c31bb58dd2d283b6fb6129f72e6a49060
tjhirani <tejashirani55@gmail.com>
2023-10-11 03:15:51 -0500

    [*] bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+    name: 'pope',
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
